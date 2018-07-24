383ccaedfae99a706c16d75bc00caf046f372f9a
Attila Sasvari <asasvari@apache.org>
2018-07-24 13:17:08 +0200

    Add demo app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # demokafkalog4jappender
+
+Demo application that sends log messages to Kafka using log4j, slf4j with KafkaLog4jAppender
---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+import org.apache.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+
+    private static final Logger logger = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] argv) {
+        logger.debug("Debug message from HelloKafkaLogger.main," );
+        logger.info("Info message from HelloKafkaLogger.main" );
+        logger.warn("Warn message from HelloKafkaLogger.main");
+        logger.error("Error message from HelloKafkaLogger.main" );
+        LogManager.shutdown();
+    }
+}
